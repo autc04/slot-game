a020199140b8d9e357050b9e1d93a96e097c29c8
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2026-04-27 02:31:10 +0200

    better biasing

--- src/core/ReelsContainer.ts
+++ src/core/ReelsContainer.ts
@@ -20,20 +20,45 @@ export default class ReelsContainer {
     }
 
     async spin(winBias: number = 0) {
-        // Overall time of spinning = shiftingDelay * this.reels.length
-        //
         const shiftingDelay = 800;
-        const start = Date.now();
-        const reelsToSpin = [...this.reels];
-        
-        for await (let value of this.infiniteSpinning(reelsToSpin, winBias)) {
-            const shiftingWaitTime = (this.reels.length - reelsToSpin.length + 1) * shiftingDelay;
-            
-            if (Date.now() >= start + shiftingWaitTime) {
-                reelsToSpin.shift();
-            }
+        const remainingReels = [...this.reels];
 
-            if (!reelsToSpin.length) break;
+        while (remainingReels.length > 0) {
+            // Spin all remaining reels for shiftingDelay ms
+            const start = Date.now();
+            do {
+                await Promise.all(remainingReels.map(r => r.spinOneTime()));
+                this.blessRNG();
+            } while (Date.now() < start + shiftingDelay);
+
+            // Stop the frontmost reel
+            const stoppedReel = remainingReels.shift()!;
+            const isLastReel = remainingReels.length === 0;
+
+            if (isLastReel && winBias < 0) {
+                // Negative bias: spin last reel extra steps to avoid a win
+                for (let extra = 0; extra < -winBias; extra++) {
+                    if (!this.checkForWin(this.reels.map(r => r.sprites[2]))) break;
+                    await stoppedReel.spinOneTime();
+                    this.blessRNG();
+                }
+            } else if (winBias > 0) {
+                if (isLastReel) {
+                    // Positive bias: spin last reel extra steps until a win occurs
+                    for (let extra = 0; extra < winBias; extra++) {
+                        if (this.checkForWin(this.reels.map(r => r.sprites[2]))) break;
+                        await stoppedReel.spinOneTime();
+                        this.blessRNG();
+                    }
+                } else {
+                    // Positive bias: spin stopped reel + remaining together while win is impossible
+                    for (let extra = 0; extra < winBias; extra++) {
+                        if (this.isWinPossible(stoppedReel)) break;
+                        await Promise.all([stoppedReel, ...remainingReels].map(r => r.spinOneTime()));
+                        this.blessRNG();
+                    }
+                }
+            }
         }
 
         // reel.sprites[2] - Middle visible symbol of the reel
@@ -41,13 +66,16 @@ export default class ReelsContainer {
         return this.checkForWin(this.reels.map(reel => reel.sprites[2]));
     }
 
-    private async* infiniteSpinning(reelsToSpin: Array<Reel>, winBias: number) {
-        while (true) {
-            const spinningPromises = reelsToSpin.map(reel => reel.spinOneTime());
-            await Promise.all(spinningPromises);
-            this.blessRNG(winBias);
-            yield;
-        }
+    private isWinPossible(lastStoppedReel: Reel): boolean {
+        const stoppedIndex = this.reels.indexOf(lastStoppedReel);
+        if (stoppedIndex < 1) return true; // First reel alone never blocks a win
+
+        const sym = (reel: Reel) =>
+            reel.sprites[2].texture.textureCacheIds[0].split('.')[0];
+        const s0 = sym(this.reels[0]);
+        const s1 = sym(this.reels[1]);
+        // Win is impossible only when two different non-wild symbols have already appeared
+        return !(s0 !== 'SYM1' && s1 !== 'SYM1' && s0 !== s1);
     }
 
     private checkForWin(symbols: Array<PIXI.Sprite>): boolean {
@@ -59,15 +87,9 @@ export default class ReelsContainer {
         return combination.size === 2 && combination.has('SYM1');
     }
 
-    private blessRNG(winBias: number) {
+    private blessRNG() {
         this.reels.forEach(reel => {
-            const pool = [...reel.textures];
-            if (winBias > 0) {
-                for (let i = 0; i < winBias; i++) pool.push(reel.textures[1]);
-            } else if (winBias < 0) {
-                for (let i = 0; i < -winBias; i++) pool.push(reel.textures[0]);
-            }
-            reel.sprites[0].texture = pool[Math.floor(Math.random() * pool.length)];
+            reel.sprites[0].texture = reel.textures[Math.floor(Math.random() * reel.textures.length)];
         });
     }
 }
